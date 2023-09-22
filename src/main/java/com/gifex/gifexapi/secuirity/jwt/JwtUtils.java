bafc513cbe890a3ec986bb3cf9fdae1b2be070ff
swsbmm <swsbmm@gmail.com>
2023-09-22 10:36:09 -0500

    feature added class of utils for jwt

---
+++ src/main/java/com/gifex/gifexapi/secuirity/jwt/JwtUtils.java
@@ -0,0 +1,78 @@
+package com.gifex.gifexapi.secuirity.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+@Slf4j
+public class JwtUtils {
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    @Value("${jwt.time.expiration}")
+    private String timeExpiration;
+
+
+    //generar token de acceso
+    public String generateAccesToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+Long.parseLong(timeExpiration)))
+                .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    //validar el token de acceso
+    public boolean isTokenValid(String token){
+        try{
+            // este decodifica el token
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignatureKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        }catch (Exception e){
+            log.error("Token invalido, error: ".concat(e.getMessage()));
+            return false;
+        }
+    }
+
+    // obtener el username del token
+    public String getUsernameFromToken(String token){
+        return getClaim(token, Claims::getSubject);
+    }
+
+    // obtener un solo Claim
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsTFunction){
+        Claims claims = extractAllClaims(token);
+        return claimsTFunction.apply(claims);
+    }
+
+    // obtener el cuerpo del token "Claims"
+    public Claims extractAllClaims (String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // obtener firma del token
+    private Key getSignatureKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
